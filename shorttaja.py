a0b6db12944521098b498ab6cbaf9cff285af743
galaxy-note7-explosion <136370990+galaxy-note7-explosion@users.noreply.github.com>
2023-06-17 19:54:51 +0900

    Update README.md

--- shorttaja.py
+++ shorttaja.py
@@ -1,8 +1,19 @@
 import random
 import time
 
-f = open('shorthangul.txt', 'w')
-
-m = random.choice(f.split('\n'))
-
+with open('shorthangul.txt', 'r', encoding='utf-8') as f:
+    data = f.readlines()
+m = random.choice(data)
+count = len(m)
 print(m)
+start = time.time()
+m1 = str(input())
+end = time.time()
+sec = end - start
+perfect = 0
+for i in range(count-1):
+    if m[i] == m1[i]:
+        perfect += 1
+print("걸린 시간 :", int(sec))
+print("타수 :", int(perfect * 60 / sec))
+print("정확도 :", int(perfect / count * 100))
